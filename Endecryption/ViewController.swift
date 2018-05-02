21604ced8cfa607bdc7df2f2ecec1ab7aa2683b0
Dove·Z <Dove@dovezdemacbook-pro.local>
2018-05-02 14:33:50 +0800

    Initial Commit

---
+++ Endecryption/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Endecryption
+//
+//  Created by Dove·Z on 2018/5/2.
+//  Copyright © 2018年 Dove·Z. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
